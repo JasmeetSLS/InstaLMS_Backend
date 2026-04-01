7b480cf9893e58c05dfb582080c5344dc8b632d7
Jassi2201 <dbmongo080@gmail.com>
2026-04-01 17:47:44 +0530

    testing ui lms final db

--- db.sql
+++ db.sql
@@ -19,83 +19,69 @@
 CREATE DATABASE IF NOT EXISTS `insta_style_lms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `insta_style_lms`;
 
--- Dumping structure for table insta_style_lms.admins
-CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'admin',
-  `status` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'active',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Dumping data for table insta_style_lms.admins: ~1 rows (approximately)
-INSERT INTO `admins` (`id`, `username`, `password`, `email`, `role`, `status`, `created_at`, `updated_at`) VALUES
-	(1, 'Admin', '$2b$12$24vr9WK4g3VJRlDslopOBuIU5tsgIVNcfEx4jhgaYrcMszO5wqyzS', 'admin@admin.com', 'admin', 'active', '2026-03-31 11:32:26', '2026-03-31 11:37:33');
-
 -- Dumping structure for table insta_style_lms.categories
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `icon_url` varchar(500) DEFAULT NULL,
-  `status` enum('active','inactive') DEFAULT 'active',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table insta_style_lms.categories: ~1 rows (approximately)
-INSERT INTO `categories` (`id`, `name`, `icon_url`, `status`, `created_at`) VALUES
-	(1, 'Test', 'file-1775029317750-523004584.jpg', 'active', '2026-04-01 07:41:57');
+-- Dumping data for table insta_style_lms.categories: ~5 rows (approximately)
+INSERT INTO `categories` (`id`, `name`, `icon_url`, `created_at`) VALUES
+	(1, 'Product', '/uploads/1775044466337-car-sales_1d8acd.avif', '2026-04-01 11:54:26'),
+	(2, 'Process', '/uploads/1775044476486-infographics0.avif', '2026-04-01 11:54:36'),
+	(3, 'Technology', '/uploads/1775044604396-row-cars_83_1_0.jpg', '2026-04-01 11:54:48'),
+	(4, 'BAT', '/uploads/1775044494997-images.jpg', '2026-04-01 11:54:55'),
+	(5, 'Soft Skills', '/uploads/1775044488001-images (1).jpg', '2026-04-01 11:56:44');
 
--- Dumping structure for table insta_style_lms.otps
-CREATE TABLE IF NOT EXISTS `otps` (
+-- Dumping structure for table insta_style_lms.post_media
+CREATE TABLE IF NOT EXISTS `post_media` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `otp_code` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `is_verified` tinyint(1) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `expires_at` timestamp NOT NULL,
+  `post_id` int NOT NULL,
+  `media_type` enum('image','video','gif') NOT NULL,
+  `media_url` varchar(500) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_user_id` (`user_id`),
-  KEY `idx_otp_code` (`otp_code`),
-  KEY `idx_is_verified` (`is_verified`),
-  KEY `idx_expires_at` (`expires_at`),
-  CONSTRAINT `fk_otp_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `post_media_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table insta_style_lms.otps: ~1 rows (approximately)
-INSERT INTO `otps` (`id`, `user_id`, `otp_code`, `is_verified`, `created_at`, `expires_at`) VALUES
-	(1, 4, '786011', 0, '2026-04-01 11:07:48', '2026-04-01 11:17:48');
+-- Dumping data for table insta_style_lms.post_media: ~11 rows (approximately)
+INSERT INTO `post_media` (`id`, `post_id`, `media_type`, `media_url`) VALUES
+	(1, 1, 'image', '/uploads/1775044987996-farm_tractor_trolley_54c9074369.png'),
+	(2, 1, 'video', '/uploads/1775044710518-SHRESTH PUDDLING MASTER_3D Animation_Hindi VO_11-12-25.mp4'),
+	(3, 2, 'image', '/uploads/1775044777269-AutoMobiles.avif'),
+	(4, 2, 'video', '/uploads/1775044777271-JK Tyre Dealer Onboarding Process Video_19-2-26.mp4'),
+	(5, 3, 'image', '/uploads/1775044710515-BR_43_BR_50_BF_32_copy_png_a64bc06916.png'),
+	(6, 3, 'video', '/uploads/1775044988016-E-BLAZE_EV Tyre_Video_Comp_7-11-25.mp4'),
+	(7, 4, 'image', '/uploads/1775045544652-image_6487327-copy.jpg'),
+	(8, 4, 'video', '/uploads/1775045544654-Ventilated seat reel.mp4'),
+	(9, 5, 'image', '/uploads/1775045707594-2434.avif'),
+	(10, 5, 'video', '/uploads/1775045707595-JK Tyre Micro Learning 01 - Tyre Construction.mp4'),
+	(11, 5, 'video', '/uploads/1775045708236-JK Tyre Micro Learning 03 - Benefits & Applications of Radial & Bias Tyre.mp4');
 
--- Dumping structure for table insta_style_lms.users
-CREATE TABLE IF NOT EXISTS `users` (
+-- Dumping structure for table insta_style_lms.posts
+CREATE TABLE IF NOT EXISTS `posts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `employee_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'user',
-  `status` enum('active','inactive') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'active',
+  `category_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text,
+  `hashtags` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `employee_id` (`employee_id`),
-  KEY `idx_email` (`email`),
-  KEY `idx_employee_id` (`employee_id`),
-  KEY `idx_status` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table insta_style_lms.users: ~4 rows (approximately)
-INSERT INTO `users` (`id`, `email`, `employee_id`, `name`, `role`, `status`, `created_at`, `updated_at`) VALUES
-	(1, 'Testing1@sls-india.com', 'EMP002', 'TestUser2', 'Sales', 'active', '2026-04-01 06:32:21', '2026-04-01 06:32:21'),
-	(2, 'Testing2@sls-india.com', 'EMP003', 'TestUser3', 'DFM', 'active', '2026-04-01 06:32:21', '2026-04-01 06:32:21'),
-	(3, 'Testing3@sls-india.com', 'EMP004', 'TestUser4', 'DSE', 'active', '2026-04-01 06:32:21', '2026-04-01 06:32:21'),
-	(4, 'jasmeet.singh@sls-india.com', 'EMP001', 'John Doe', 'User', 'active', '2026-04-01 11:07:33', '2026-04-01 11:07:33');
+-- Dumping data for table insta_style_lms.posts: ~5 rows (approximately)
+INSERT INTO `posts` (`id`, `category_id`, `title`, `content`, `hashtags`, `created_at`) VALUES
+	(1, 1, 'Farm Tyre', 'Farm Tyre', '#Farm Tyre #JK Tyre', '2026-04-01 11:58:31'),
+	(2, 2, 'Onboarding Process', 'Onboarding Process', '#Onboarding Process #JK Tyre', '2026-04-01 11:59:37'),
+	(3, 1, 'E-Blaze', 'E-Blaze', '#E-Blaze', '2026-04-01 12:03:08'),
+	(4, 3, 'Ventilated Seat', 'Ventilated Seat', '#Ventilated Seat  #Seat', '2026-04-01 12:12:25'),
+	(5, 1, 'MicroLearning', 'MicroLearning', '#MicroLearning #JK Tyre', '2026-04-01 12:15:08');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
